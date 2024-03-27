94c16ef835c6b815c343880f3d156778db407fcd
alextregubov <tgnwest@gmail.com>
2024-03-27 07:21:36 +0300

    added di

--- api/public/index.php
+++ api/public/index.php
@@ -2,15 +2,26 @@
 
 declare(strict_types=1);
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
 
-$app->addErrorMiddleware((bool)getenv('APP_DEBUG'), true, true);
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool)getenv('APP_DEBUG'),
+    ],
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::createFromContainer($container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write('{}');

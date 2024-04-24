d07b631cbaac356f9de29898b5fd1b2b64f40e6f
alextregubov <tgnwest@gmail.com>
2024-04-24 07:17:39 +0300

    added functional test

---
+++ api/tests/Functional/HomeTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Functional;
+
+/**
+ * @covers HomeTest
+ */
+class HomeTest extends WebTestCase
+{
+    public function testSuccess(): void
+    {
+        $response = $this->app()->handle(self::json('GET', '/'));
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('application/json', $response->getHeaderLine('Content-Type'));
+        self::assertEquals('{}', (string)$response->getBody());
+    }
+
+    public function testMethod(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/'));
+
+        self::assertEquals(405, $response->getStatusCode());
+    }
+}
